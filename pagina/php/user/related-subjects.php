7d4cad70943a85454a2ffc67b819fe74d485add8
TMendoza <104092353+Mendox41@users.noreply.github.com>
2024-04-26 20:14:55 -0300

    funciona related_subject

--- pagina/php/user/related-subjects.php
+++ pagina/php/user/related-subjects.php
@@ -9,14 +9,13 @@ include(__DIR__ . "/../error_stmt/errorFunctions.php");
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$id_usuario = isset($_POST['id_usuario ']) ? $_POST['id_usuario'] : null;
-
+$id_usuario = isset($_POST['id_usuario']) ? $_POST['id_usuario'] : null;
 
 $result = new stdClass();
 $result->success = false;
 
 if (empty($id_usuario)) {
-    error_request($result, "No hay ningun id asignado");
+    error_request($result, "El id ingresado esta vacio  $id_usuario");
 }
 
 $db_name = "300hs_laborales";
@@ -36,42 +35,39 @@ if (!$stmt->execute()) {
 
 // Manejo de resultados
 $stmt->bind_result($id_curcom, $id_relacion, $carrera, $anio, $semestre, $materia, $id_profesor, $legajo, $apellido_nombre, $turno, $id_comision, $comision);
-$exists = $stmt->fetch();
-
-
-$stmt->close();
-
 
 $relacion = [];
 
-if (!$stmt->fetch()) {
-    error_request($result, "No hay resultados para ese ID: " . $id_user);
-} else {
-
-   while ($fila = $stmt->fetch()) {
+while ($stmt->fetch()) {
     $datoRelacion = new stdClass();
 
-    $datoRelacion -> id_curcom = $id_curcom;
-    $datoRelacion ->id_relacion = $id_relacion;
-    $datoRelacion ->carrera = $carrera;
-    $datoRelacion ->anio = $anio;
-    $datoRelacion ->semestre = $semestre;
-    $datoRelacion ->materia = $materia;
-    $datoRelacion ->id_profesor = $id_profesor;
-    $datoRelacion ->legajo = $legajo;
-    $datoRelacion ->apellido_nombre = $apellido_nombre;
-    $datoRelacion ->turno = $turno;
-    $datoRelacion ->id_comision = $id_comision;
-    $datoRelacion ->$comision = $comision;
+    $datoRelacion->id_curcom = $id_curcom;
+    $datoRelacion->id_relacion = $id_relacion;
+    $datoRelacion->carrera = $carrera;
+    $datoRelacion->anio = $anio;
+    $datoRelacion->semestre = $semestre;
+    $datoRelacion->materia = $materia;
+    $datoRelacion->id_profesor = $id_profesor;
+    $datoRelacion->legajo = $legajo;
+    $datoRelacion->apellido_nombre = $apellido_nombre;
+    $datoRelacion->turno = $turno;
+    $datoRelacion->id_comision = $id_comision;
+    $datoRelacion->comision = $comision;
 
     array_push($relacion, $datoRelacion);
-   }
-
-   $objRelacion = new stdClass();
-   $objRelacion -> relacion = $relacion; 
-   $result -> respuesta = $objRelacion;
-   $result->success = true;          
 }
 
+if (empty($relacion)) {
+    error_request($result, "No hay resultados para ese ID: " . $id_usuario);
+} else {
+    $objRelacion = new stdClass();
+    $objRelacion->relacion = $relacion;
+    $result->respuesta = $objRelacion;
+    $result->success = true;
+}
+
+$stmt->close();
+
 echo json_encode($result);
+
 ?>
