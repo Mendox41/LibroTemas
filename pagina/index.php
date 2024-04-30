755543208f20c047414059996b92ab0badc6e55b
GatitoEnojado <ibottaro99@gmail.com>
2024-04-30 02:31:17 -0300

    Modificando y agregando manejo de sesion y redireccionamiento en el index principal

--- pagina/index.php
+++ pagina/index.php
@@ -4,8 +4,11 @@ session_start();
 if (!isset($_SESSION['idSesion'])) {
     header('Location: templates/login.php');
     exit();
+}else if($_SESSION['admin'] ===  1){
+    header('Location: templates/admin/home.php');
+    exit();
 } else {
-    header('Location: templates/home.php');
+    header('Location: templates/user/home.php');
     exit();
 }
 ?>
