470ff634d929abaef005ad8618e59d2a4c5d5a39
GatitoEnojado <ibottaro99@gmail.com>
2024-04-30 02:33:40 -0300

    Modificando archivo js de validacion de login

---
+++ pagina/js/login.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+
+    $("#errorMessage").empty();
+    $("#errorMessage").addClass("oculto");
+
+    var loginForm = document.getElementById("loginForm");
+
+    var loginBtn = document.getElementById("loginBtn");
+
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    loginBtn.disabled = true;
+
+    loginForm.onkeyup = function () {
+
+        if (email.value !== "" && password.value !== "") {
+            loginBtn.disabled = false;
+
+        } else {
+            loginBtn.disabled = true;
+
+        }
+    };
+
+    $('#loginBtn').click(function (event) {
+        event.preventDefault();
+
+
+        var datosLoginForm = $('#loginForm').serialize();
+
+        var objAjax = $.ajax({
+            type: "post",
+            url: "../php/session/processLogin.php",
+            data: datosLoginForm,
+            success: function (respuesta) {
+                var data = JSON.parse(respuesta);
+                if (data.success == true) {
+                    alert("ENTRAMOSSSS");
+                    window.location.href = "../templates/user/home.php";
+                } else {
+                    $("#errorMessage").empty();
+                    $("#errorMessage").text(data.message);
+                    $("#errorMessage").removeClass("oculto");
+                }
+
+            }, error: function (error) {
+                console.log(error);
+            }
+
+
+        });
+
+    });
+
+    $('#registerBtn').click(function (event) {
+        event.preventDefault();
+
+        window.location.href = "register.html";
+
+    });
+
+
+});
