5f98e989abd0e7b8cb384c0bc6640acfa7d3ca92
GatitoEnojado <ibottaro99@gmail.com>
2024-05-02 14:20:04 -0300

    Modificando archivo de cerrar sesion

--- pagina/php/session/destroy-session.php
+++ pagina/php/session/destroy-session.php
@@ -1,15 +1,12 @@
 <?php
 
-if (!isset($_SESSION['idSession'])) {
-    header('location: ../../templates/login.php');
-} else {
+
     session_start();
     session_unset();
     session_destroy();
 
     header('location: ../../templates/login.php');
     exit();
-}
 
 
 ?>
