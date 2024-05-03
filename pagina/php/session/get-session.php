296c39718d8705738582f50126e06b8b13b51289
GatitoEnojado <ibottaro99@gmail.com>
2024-05-03 14:04:59 -0300

    Modificando archivo get-session.php

--- pagina/php/session/get-session.php
+++ pagina/php/session/get-session.php
@@ -2,21 +2,19 @@
 
 
 session_start();
+$variablesSession = new stdClass();
+$variablesSession-> success = false;
 
-if (!isset($_SESSION['idSession'])) {
-    header('location: ../../templates/login.php');
-    exit();
-} else {
-    $variablesSession = new stdClass();
+if (isset($_SESSION['idSession'])) {
     $variablesSession->id_usuario = $_SESSION['id_usuario'];
     $variablesSession->usuario = $_SESSION['usuario'];
     $variablesSession->active = $_SESSION['active'];
     $variablesSession->admin = $_SESSION['admin'];
+    $variablesSession->success = true;
+    
+} 
 
-    echo json_encode($variablesSession);
-
-}
-
+echo json_encode($variablesSession);
 
 
 ?>
