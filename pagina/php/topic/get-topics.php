dfb09ba8f17f3a20f4bd3e0d8147d9708896b7df
TMendoza <104092353+Mendox41@users.noreply.github.com>
2024-06-17 22:48:09 -0300

    Correccion get-topics

--- pagina/php/topic/get-topics.php
+++ pagina/php/topic/get-topics.php
@@ -6,154 +6,147 @@ include(__DIR__ . "/../database/conection.php");
 // Incluir las funciones de error
 include(__DIR__ . "/../error_stmt/errorFunctions.php");
 
-|
-
 $nombre_carrera = isset($_POST["nombre-carrera"]) ? $_POST["nombre-carrera"] : '';
 $anio_carrera = isset($_POST["anio-carrera"]) ? $_POST["anio-carrera"] : '';
 $nombre_materia = isset($_POST["nombre-materia"]) ? $_POST["nombre-materia"] : '';
 $comision = isset($_POST["comision"]) ? $_POST["comision"] : '';
 $turno = isset($_POST["turno"]) ? $_POST["turno"] : '';
-$nombre_usuario = isset($_POST["nombre-usuario"]) ? $_POST["nombre-usuario"] : '';
 $nombre_profesor = isset($_POST["nombre-profesor"]) ? $_POST["nombre-profesor"] : '';
 $apellido_profesor = isset($_POST["apellido-profesor"]) ? $_POST["apellido-profesor"] : '';
 $fecha_tema = isset($_POST["fecha-tema"]) ? $_POST["fecha-tema"] : '';
 $titulo_tema = isset($_POST["titulo-tema"]) ? $_POST["titulo-tema"] : '';
 $descripcion_tema = isset($_POST["descripcion-tema"]) ? $_POST["descripcion-tema"] : '';
 
-// $inputOrdenar = isset($_POST["inputOrdenar"]) ? $_POST["inputOrdenar"] : '';
-
 $result = new stdClass();
 $result->success = false;
 
-// Armo la cosulta     
-$sql = "SELECT T1.id_libro_tema, T1.tema, T1.descripcion, T1.fecha, T6.carrera, T7.anio ,T5.materia, comision, turno, T3.apellido, T3.nombre  
-    FROM libro_temas AS T1
-    INNER JOIN cursos AS T2 ON T2.id_curso = T1.id_curso
-    INNER JOIN profesores AS T3 ON T3.id_profesor = T1.id_profesor
-    INNER JOIN relaciones AS T4 ON T4.id_relacion = T1.id_relacion
-    INNER JOIN materias AS T5 ON T5.id_materia = T4.id_materia
-    INNER JOIN carreras AS T6 ON T6.id_carrera = T4.id_carrera
-    INNER JOIN anios AS T7 ON T7.id_anio = T4.id_anio
-    INNER JOIN comisiones AS T8 ON T8.id_comision = T2.id_comision 
-    INNER JOIN turnos AS T9 ON T9.id_turno = T2.id_turno 
-    INNER JOIN usuarios AS T10 ON T10.id_usuario = T3.id_usuario
-    WHERE 1=1";
+$databaseName = "300hs_laborales";
+mysqli_select_db($conn, $databaseName);
 
+// Armo la consulta     
+$sql = "SELECT T1.id_libro_tema, T1.tema, T1.descripcion, T1.fecha, T6.carrera, T7.anio, T5.materia, T8.comision, T9.turno, T3.apellido, T3.nombre  
+        FROM libro_temas AS T1
+        INNER JOIN cursos AS T2 ON T2.id_curso = T1.id_curso
+        INNER JOIN profesores AS T3 ON T3.id_profesor = T1.id_profesor
+        INNER JOIN relaciones AS T4 ON T4.id_relacion = T1.id_relacion
+        INNER JOIN materias AS T5 ON T5.id_materia = T4.id_materia
+        INNER JOIN carreras AS T6 ON T6.id_carrera = T4.id_carrera
+        INNER JOIN anios AS T7 ON T7.id_anio = T4.id_anio
+        INNER JOIN comisiones AS T8 ON T8.id_comision = T2.id_comision 
+        INNER JOIN turnos AS T9 ON T9.id_turno = T2.id_turno
+        WHERE 1=1";
+
+$parameters = [];
+$types = "";
 
 if (!empty($nombre_carrera)) {
-    $sql .= " AND T6.carrera LIKE CONCAT('%',:carrera,'%')";
+    $sql .= " AND T6.carrera LIKE ?";
+    $parameters[] = "%$nombre_carrera%";
+    $types .= "s";
 }
 if (!empty($anio_carrera)) {
-    $sql .= " AND T7.anio LIKE CONCAT('%',:anio,'%')";
+    $sql .= " AND T7.anio LIKE ?";
+    $parameters[] = "%$anio_carrera%";
+    $types .= "s";
 }
 if (!empty($nombre_materia)) {
-    $sql .= " AND T5.materia LIKE CONCAT('%',:materia,'%')";
+    $sql .= " AND T5.materia LIKE ?";
+    $parameters[] = "%$nombre_materia%";
+    $types .= "s";
 }
 if (!empty($comision)) {
-    $sql .= " AND T8.comision LIKE CONCAT('%',:comision,'%')";
+    $sql .= " AND T8.comision LIKE ?";
+    $parameters[] = "%$comision%";
+    $types .= "s";
 }
 if (!empty($turno)) {
-    $sql .= " AND T9.turno LIKE CONCAT('%',:turno,'%')";
-}
-if (!empty($nombre_usuario)) {
-    $sql .= " AND T10.usuario LIKE CONCAT('%',:usuario,'%')";
-}
-if (!empty($nombre_profesor)) {
-    $sql .= " AND T3.nombre LIKE CONCAT('%',:nombre,'%')";
-}
-if (!empty($apellido_profesor)) {
-    $sql .= " AND T3.apellido LIKE CONCAT('%',:apellido,'%')";
-}
-if (!empty($fecha_tema)) {
-    $sql .= " AND fecha LIKE CONCAT('%',:fecha,'%')";
-}
-if (!empty($titulo_tema)) {
-    $sql .= " AND tema LIKE CONCAT('%',:tema,'%')";
-}
-if (!empty($descripcion_tema)) {
-    $sql .= " AND descripcion LIKE CONCAT('%',:descripcion,'%')";
-}
-// if (!empty($inputOrdenar)) {
-//     $sql .= " ORDER BY $inputOrdenar";
-// }
-
-$stmt = $dbh->prepare($sql);
-
-// Vinculo los parámetros 
-if (!empty($nombre_carrera)) {
-    $stmt->bindParam(':carrera', $nombre_carrera);
-}
-if (!empty($anio_carrera)) {
-    $stmt->bindParam(':anio', $anio_carrera);
-}
-if (!empty($nombre_materia)) {
-    $stmt->bindParam(':materia', $nombre_materia);
-}
-if (!empty($comision)) {
-    $stmt->bindParam(':comision', $comision);
-}
-if (!empty($turno)) {
-    $stmt->bindParam(':turno', $turno);
-}
-if (!empty($nombre_usuario)) {
-    $stmt->bindParam(':usuario', $nombre_usuario);
+    $sql .= " AND T9.turno LIKE ?";
+    $parameters[] = "%$turno%";
+    $types .= "s";
 }
 if (!empty($nombre_profesor)) {
-    $stmt->bindParam(':nombre', $nombre_profesor);
+    $sql .= " AND T3.nombre LIKE ?";
+    $parameters[] = "%$nombre_profesor%";
+    $types .= "s";
 }
 if (!empty($apellido_profesor)) {
-    $stmt->bindParam(':apellido', $apellido_profesor);
+    $sql .= " AND T3.apellido LIKE ?";
+    $parameters[] = "%$apellido_profesor%";
+    $types .= "s";
 }
 if (!empty($fecha_tema)) {
-    $stmt->bindParam(':fecha', $fecha_tema);
+    $sql .= " AND T1.fecha LIKE ?";
+    $parameters[] = "%$fecha_tema%";
+    $types .= "s";
 }
 if (!empty($titulo_tema)) {
-    $stmt->bindParam(':tema', $titulo_tema);
+    $sql .= " AND T1.tema LIKE ?";
+    $parameters[] = "%$titulo_tema%";
+    $types .= "s";
 }
 if (!empty($descripcion_tema)) {
-    $stmt->bindParam(':descripcion', $descripcion_tema);
+    $sql .= " AND T1.descripcion LIKE ?";
+    $parameters[] = "%$descripcion_tema%";
+    $types .= "s";
 }
 
+$stmt = $conn->prepare($sql);
 
-$stmt->setFetchMode(PDO::FETCH_ASSOC);
-$stmt->execute();
-if (!empty($nombre_carrera)) {
-    $stmt->bindParam(':carrera', $nombre_carrera);
+// Verificar si la preparación de la consulta fue exitosa
+if (!$stmt) {
+    error_stmt($result, "Error preparing the query: " . $conn->error, $stmt, $conn);
+    echo json_encode($result);
+    exit;
 }
+
+// Vincular los parámetros
+if (!empty($parameters)) {
+    $stmt->bind_param($types, ...$parameters);
+}
+
+// Ejecutar la consulta y verificar si fue exitosa
+if (!$stmt->execute()) {
+    error_stmt($result, "Error executing the query: " . $stmt->error, $stmt, $conn);
+    echo json_encode($result);
+    exit;
+}
+
+$stmt->store_result();
+$stmt->bind_result($id_libro_tema, $tema, $descripcion, $fecha, $carrera, $anio, $materia, $comision, $turno, $apellido, $nombre);
+
 $topic = [];
-while ($fila = $stmt->fetch()) {
+$cont = 0;
+
+while ($stmt->fetch()) {
     $objTopic = new stdClass();
-    $objTopic->carrera = $fila["carrera"];
-    $objTopic->anio = $fila["anio"];
-    $objTopic->materia = $fila["materia"];
-    $objTopic->comision = $fila["comision"];
-    $objTopic->turno = $fila["turno"];
-    $objTopic->usuario = $fila["usuario"];
-    $objTopic->nombre = $fila["nombre"];
-    $objTopic->apellido = $fila["apellido"];
-    $objTopic->fecha = $fila["fecha"];
-    $objTopic->tema = $fila["tema"];
-    $objTopic->descripcion = $fila["descripcion"];
-    // $objTopic->archivo_binario = $fila["archivo_binario"];
+    $objTopic->id_libro_tema = $id_libro_tema;
+    $objTopic->carrera = $carrera;
+    $objTopic->anio = $anio;
+    $objTopic->materia = $materia;
+    $objTopic->comision = $comision;
+    $objTopic->turno = $turno;
+    $objTopic->nombre = $nombre;
+    $objTopic->apellido = $apellido;
+    $objTopic->fecha = $fecha;
+    $objTopic->tema = $tema;
+    $objTopic->descripcion = $descripcion;
+
+    $cont += 1;
 
     array_push($topic, $objTopic);
 }
 
 if (empty($topic)) {
-    error_stmt($result, "Error executing the query: " . $conn->error, $stmt, $conn);
-    echo json_encode($result);
-    exit();
+    error_stmt($result, "No topics found", $stmt, $conn);
+} else {
+    $result->respuesta = $topic;
+    $result->cant_temas = $cont;
+    $result->success = true;
 }
 
+$stmt->close();
+$conn->close();
 
-$objTopic = new stdClass();
-$objTopic->topic = $topic;
-$objTopic->cont = count($topic);
-$result->respuesta = $objRelacion;
-$result->success = true;
-$dbh = null;
-
-$result->success = true;
 echo json_encode($result);
 
 ?>
