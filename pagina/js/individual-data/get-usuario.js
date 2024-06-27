5b9adc21fe1754f4c9d4dcde801d36f3783ed568
GatitoEnojado <ibottaro99@gmail.com>
2024-06-27 13:38:35 -0300

    Agregando archivo js para oftencion de id de usuario

--- pagina/js/individual-data/get-usuario.js
+++ pagina/js/individual-data/get-usuario.js
@@ -1,22 +1,19 @@
 export function get_usuario() {
-    // variables para almacenar valores de la sesion
-    var id_usuario;
- 
-    $.ajax({
-        url: '../../php/session/get-session.php',
-        type: 'GET',
-
-        success: function (respuesta) {
-
-            var dataSession = JSON.parse(respuesta);
-
-            if (dataSession.success == true && dataSession.id_usuario !== null && dataSession.id_usuario !== undefined) {
-                id_usuario = dataSession.id_usuario;
-                return id_usuario;
-            } else {
-                return false;
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: '../../php/session/get-session.php',
+            type: 'GET',
+            success: function (respuesta) {
+                var dataSession = JSON.parse(respuesta);
+                if (dataSession.success == true && dataSession.id_usuario !== null && dataSession.id_usuario !== undefined) {
+                    resolve(dataSession.id_usuario);
+                } else {
+                    resolve(false);
+                }
+            },
+            error: function(error) {
+                reject(error);
             }
-
-        }
-    })
-};
+        });
+    });
+}
