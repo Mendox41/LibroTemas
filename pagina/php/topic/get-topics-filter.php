425170fffe96b392d5030096bbf213e0b4eea819
TMendoza <104092353+Mendox41@users.noreply.github.com>
2024-07-10 09:45:30 -0300

    Actualizacion get-topic-filter

--- pagina/php/topic/get-topics-filter.php
+++ pagina/php/topic/get-topics-filter.php
@@ -13,9 +13,13 @@ $anio_carrera = isset($_POST["anio_carrera"]) ? $_POST["anio_carrera"] : '';
 $nombre_materia = isset($_POST["nombre_materia"]) ? $_POST["nombre_materia"] : '';
 $comision = isset($_POST["comision"]) ? $_POST["comision"] : '';
 $turno = isset($_POST["turno"]) ? $_POST["turno"] : '';
+$usuario = isset($_POST["usuario"]) ? $_POST["usuario"] : '';
 $nombre_profesor = isset($_POST["nombre_profesor"]) ? $_POST["nombre_profesor"] : '';
 $apellido_profesor = isset($_POST["apellido_profesor"]) ? $_POST["apellido_profesor"] : '';
-$fecha_tema = isset($_POST["fecha_tema"]) ? $_POST["fecha_tema"] : '';
+$ciclo_lectivo = isset($_POST["ciclo_lectivo"]) ? $_POST["ciclo_lectivo"] : '';
+$semestre = isset($_POST["semestre"]) ? $_POST["semestre"] : '';
+$fecha_desde = isset($_POST["fecha_desde"]) ? $_POST["fecha_desde"] : '';
+$fecha_hasta = isset($_POST["fecha_hasta"]) ? $_POST["fecha_hasta"] : '';
 $titulo_tema = isset($_POST["titulo_tema"]) ? $_POST["titulo_tema"] : '';
 $descripcion_tema = isset($_POST["descripcion_tema"]) ? $_POST["descripcion_tema"] : '';
 
@@ -38,12 +42,14 @@ $sql = "SELECT T1.id_libro_tema, T1.tema, T1.descripcion, T1.fecha, T6.carrera, 
         INNER JOIN anios AS T7 ON T7.id_anio = T4.id_anio
         INNER JOIN comisiones AS T8 ON T8.id_comision = T2.id_comision 
         INNER JOIN turnos AS T9 ON T9.id_turno = T2.id_turno
+        INNER JOIN usuarios AS T10 ON T10.id_usuario = T3.id_usuario
+        INNER JOIN semestres AS T11 ON T11.id_semestre = T2.id_semestre
         WHERE 1=1";
 
 $parameters = [];
 $types = "";
 
-// Verificar y añadir condiciones dinámicas a la consulta
+// Verificar y añadir condiciones dinámixamcas a la consulta
 if (!empty($nombre_carrera)) {
     $sql .= " AND T6.carrera LIKE ?";
     $parameters[] = "%" . $nombre_carrera . "%";
@@ -69,6 +75,11 @@ if (!empty($turno)) {
     $parameters[] = "%" . $turno . "%";
     $types .= "s";
 }
+if (!empty($usuario)) {
+    $sql .= " AND T10.usuario LIKE ?";
+    $parameters[] = "%" . $usuario . "%";
+    $types .= "s";
+}
 if (!empty($nombre_profesor)) {
     $sql .= " AND T3.nombre LIKE ?";
     $parameters[] = "%" . $nombre_profesor . "%";
@@ -79,10 +90,34 @@ if (!empty($apellido_profesor)) {
     $parameters[] = "%" . $apellido_profesor . "%";
     $types .= "s";
 }
-if (!empty($fecha_tema)) {
-    $sql .= " AND T1.fecha LIKE ?";
-    $parameters[] = "%" . $fecha_tema . "%";
+if (!empty($ciclo_lectivo)) {
+    $sql .= " AND T2.c_anio LIKE ?";
+    $parameters[] = "%" . $ciclo_lectivo . "%";
     $types .= "s";
+}
+if (!empty($semestre)) {
+    $sql .= " AND T1.semestre LIKE ?";
+    $parameters[] = "%" . $semestre . "%";
+    $types .= "s";
+}
+if (!empty($fecha_desde) && !empty($fecha_hasta)) {
+    $sql .= " AND T1.fecha BETWEEN ? AND ?";
+    $parameters[] = $fecha_desde;
+    $parameters[] = $fecha_hasta;
+    $types .= "ss";
+}
+elseif (!empty($fecha_desde)) {
+    $sql .= " AND T1.fecha BETWEEN ? AND CURDATE()";
+    $parameters[] = $fecha_desde;
+    $types .= "s";
+}
+elseif (!empty($fecha_hasta)) {
+    $sql .= " AND T1.fecha BETWEEN CONCAT(YEAR(CURDATE()),'-01-01') AND ?";
+    $parameters[] = $fecha_hasta;
+    $types .= "s";
+}
+else {
+    $sql .= " AND T1.fecha BETWEEN CONCAT(YEAR(CURDATE()),'-01-01') AND CURDATE()";
 }
 if (!empty($titulo_tema)) {
     $sql .= " AND T1.tema LIKE ?";
