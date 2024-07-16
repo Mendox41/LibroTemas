854fea42ddc5c4819587a4ac29a16a7570d4e57c
TMendoza <104092353+Mendox41@users.noreply.github.com>
2024-07-16 14:43:52 -0300

    Creacion get_profesires_id

---
+++ pagina/php/individual_data/get_profesor_id.php
@@ -0,0 +1,55 @@
+<?php
+
+// Incluir el archivo de conexión a la base de datos
+include(__DIR__ . "/../database/conection.php");
+
+// Incluir las funciones de error
+include(__DIR__ . "/../error_stmt/errorFunctions.php");
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$result = new stdClass();
+$result->success = false;
+
+$db_name = "300hs_laborales";
+mysqli_select_db($conn, $db_name);
+
+// Llamada al procedimiento almacenado
+$stmt = $conn->prepare("SELECT id_profesor, apellido, nombre FROM profesores;");
+if (!$stmt) {
+    error_stmt($result, "Error preparing the query: " . $conn->error, $stmt, $conn);
+}
+
+if (!$stmt->execute()) {
+    error_stmt($result, "Error executing the query: " . $conn->error, $stmt, $conn);
+}
+
+// Manejo de resultados
+$stmt->bind_result($id_profesor, $apellido, $nombre);
+
+$profesores = [];
+
+while ($stmt->fetch()) {
+    $datoRelacion = new stdClass();
+
+    $datoRelacion->id_profesor = $id_profesor;
+    $datoRelacion->apellido_nombre = $apellido.", ".$nombre;
+
+    array_push($profesores, $datoRelacion);
+}
+
+if (empty($profesores)) {
+    error_request($result, "No hay profesores registrados");
+} else {
+    $objRelacion = new stdClass();
+    $objRelacion->profesores = $profesores;
+    $result->respuesta = $objRelacion;
+    $result->success = true;
+}
+
+$stmt->close();
+$conn->close();
+
+echo json_encode($result);
+?>
