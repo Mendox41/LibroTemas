fd277aaa9223f7d455af8333738638dc9252d55e
TMendoza <104092353+Mendox41@users.noreply.github.com>
2024-07-17 05:19:51 -0300

    Modificacion New_user y eliminate_user

--- pagina/php/user/new-user.php
+++ pagina/php/user/new-user.php
@@ -1,20 +1,19 @@
 <?php
 
 include(__DIR__ . "/../database/conection.php");
-include ("validation.php");
+include("validation.php");
 include(__DIR__ . "/../error_stmt/errorFunctions.php");
-
 
 $usuario = !empty($_POST['usuario']) ? $_POST['usuario'] : null;
 $plain_password = !empty($_POST['pass']) ? $_POST['pass'] : null;
-
-
+$id_profesor = isset($_POST["id_profesor"]) ? $_POST["id_profesor"] : '';
+$admin = isset($_POST["admin"]) ? $_POST["admin"] : '';
 
 $result = new stdClass();
 $result->success = true;
 
-if(($usuario === null) || ($plain_password === null)) {
-    error_request($result, "All field must be complete");
+if (($usuario === null) || ($plain_password === null) || ($admin === '') || ($id_profesor === '')) {
+    error_request($result, "Todos los campos deben ser completados");
 }
 
 $databaseName = "300hs_laborales";
@@ -23,21 +22,33 @@ mysqli_select_db($conn, $databaseName);
 $hashed_password = password_hash($plain_password, PASSWORD_DEFAULT);
 
 # Insert instruction
-$insertUserQuery = "INSERT INTO usuarios (usuario, pass, isActive, isAdmin) VALUES (?, ?, 1, 0)";
+$insertUserQuery = "CALL new_user(?,?,?,?,@usuario_creado, @mensaje)";
 $stmt = $conn->prepare($insertUserQuery);
 
 if (!$stmt) {
-    error_stmt($result, "Error preparing the query: " . $conn->error, $stmt, $conn);
+    error_stmt($result, "Error preparando la consulta: " . $conn->error, $stmt, $conn);
 }
 
-$stmt->bind_param("ss", $usuario, $hashed_password);
+$stmt->bind_param("ssii", $usuario, $hashed_password, $id_profesor, $admin);
 
 if (!$stmt->execute()) {
-    error_stmt($result, "Error executing the query: " . $conn->error, $stmt, $conn);
+    error_stmt($result, "Error ejecutando la consulta: " . $conn->error, $stmt, $conn);
 }
 
+// Obtener los valores de salida del procedimiento almacenado
+$select = $conn->query("SELECT @usuario_creado AS usuario_creado, @mensaje AS mensaje");
+$result_row = $select->fetch_assoc();
+
 $stmt->close();
-$conn->close(); 
+$conn->close();
+
+if ($result_row['usuario_creado']) {
+    $result->message = $result_row['mensaje'];
+    $result->success = true;
+} else {
+    $result->message = $result_row['mensaje'];
+    $result->success = false;
+}
 
 echo json_encode($result);
 
